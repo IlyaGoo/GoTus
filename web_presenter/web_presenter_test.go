925697af9b499594170a656d87df6f8b3fdd25ca
IlyaGoo <ilyagooooo@yandex.ru>
2021-10-23 16:39:25 +0500

    First test example

---
+++ web_presenter/web_presenter_test.go
@@ -0,0 +1,14 @@
+package web_presenter
+
+import (
+	"testing"
+)
+
+func TestPortSettingInConstructor(t *testing.T) {
+	port := "25565"
+	webPresenter := NewWebPresenter(port)
+
+	if webPresenter.port != port {
+		t.Fatalf(`WebPresenter set port error`)
+	}
+}
